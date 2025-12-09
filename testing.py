3c2ae8795de14c37dded6741260152cf68a64f65
navarr393 <navarrod253@gmail.com>
2025-12-09 14:19:31 -0800

    implemented SimpleTokenizerV2

--- testing.py
+++ testing.py
@@ -15,13 +15,13 @@ result = [item for item in result if item.strip()]
 print(result)
 
 # apply it to raw text
-processed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-processed = [item for item in processed if item.strip()]
-print(len(processed))
-print(processed[:30])
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
 
 # tokens are sorted alphabetically and given a  token ID
-all_words = sorted(set(processed)) # no repeaded words is what a set returns
+all_words = sorted(set(preprocessed)) # no repeaded words is what a set returns
 vocab_size = len(all_words)
 print(vocab_size)
 
@@ -29,13 +29,14 @@ print(vocab_size)
 vocab = {token:integer for integer, token in enumerate(all_words)}
 print(vocab.items())
 
-# print the first 50
+# print the first 50, we use enumerate to have a counter and know when to stop
 for i, item in enumerate(vocab.items()):
     print(item)
     if i >= 50:
         break
 
 from simple_tokenizer import SimpleTokenizerV1
+
 tokenizer = SimpleTokenizerV1(vocab)
 text = """"It's the last he painted, you know," 
        Mrs. Gisburn said with pardonable pride."""
@@ -47,5 +48,33 @@ print(ids)
 print(tokenizer.decode(ids))
 
 # example 2 with error
-text = "Hello, do you like tea?"
+text = "do you like tea?"
 print(tokenizer.encode(text))
+
+all_tokens = sorted(list(set(preprocessed))) # make a sorted list of unique tokens from preprocessed
+all_tokens.extend(["<|endoftext|>", "<|unk|>"]) # add  the 2 new special tokens
+vocab = {token:integer for integer, token in enumerate(all_tokens)} # create vocab dictionary mapping all tokens to integer IDs
+
+print(len(vocab.items()))
+
+for i, item in enumerate(list(vocab.items())[-5:]): # print the last 5 items 
+    print(item)
+
+# the following will fail, this is why we need enumerate()
+# for i, item in vocab.items()[-5:]:
+#     print(item)
+
+# test tokenizer_v2
+from simple_tokenizer_v2 import SimpleTokenizerV2
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2)) # put the string " <|endoftext|> " between both text1 and text2
+print(text)
+
+# tokenize the sample text
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+
+# detokenize the sample text
+print(tokenizer.decode(tokenizer.encode(text)))
