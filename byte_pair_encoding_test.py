abb56f90f83163d7ab1b37f470923ef36527f166
navarr393 <navarrod253@gmail.com>
2025-12-18 13:09:16 -0800

    finished Byte Pair Encoding Example

--- byte_pair_encoding_test.py
+++ byte_pair_encoding_test.py
@@ -21,4 +21,31 @@ print(integers)
 
 # turn back into strings
 strings = tokenizer.decode(integers)
-print(strings)
+print(strings) 
+
+# encode the .txt with the new set from gpt-2
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+
+# remove the first 50 tokens from the dataset
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size] # input 
+y = enc_sample[1:context_size+1] # target, shift by one but also predict the next one by adding 1
+print(f"x: {x}")
+print(f"y:     {y}")
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i] # everything up to i but not including index i
+    desired = enc_sample[i]  # single token at index i, i is included by itself
+    print(context, "---->", desired)
+
+# same example but convert the token ids into text
+for i in range(1, context_size+1):
+    context = enc_sample[:i] # always a list 
+    desired = enc_sample[i]  # always a single integer
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired])) # must pass a list this is why we do [desired]
